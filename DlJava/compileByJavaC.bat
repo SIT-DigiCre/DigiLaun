25bbafc75036890c6395f3006c56502833a50710
TkoolerLufar <19635219+TkoolerLufar@users.noreply.github.com>
2020-11-14 03:35:24 +0900

    バッチのドキュメント出力に作者追加と文字コード明示

--- DlJava/compileByJavaC.bat
+++ DlJava/compileByJavaC.bat
@@ -1,7 +1,7 @@
 rmdir /S /Q jbin jdoc
 mkdir jbin jdoc
 javac   -d jbin -encoding UTF-8 -source 1.6 -target 1.6 src\net\digicre\digilaun\*.java src\net\digicre\digilaun\work\settable\*.java src\net\digicre\digilaun\work\*.java src\net\digicre\digilaun\config\*.java src\net\digicre\digilaun\config\regworks\*.java
-javadoc -d jdoc -encoding UTF-8 -source 1.6 -private -sourcepath src -subpackages net.digicre.digilaun -quiet
+javadoc -d jdoc -encoding UTF-8 -source 1.6 -docencoding UTF-8 -author -private -sourcepath src -subpackages net.digicre.digilaun -quiet
 cd jbin
 jar cfe ..\DigiLaun.jar net.digicre.digilaun.DigiLaun .
 PAUSE

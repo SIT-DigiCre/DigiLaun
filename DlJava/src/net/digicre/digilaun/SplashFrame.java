829e4cef9aa0b2bf091123701edd10473f15e822
TkoolerLufar <19635219+TkoolerLufar@users.noreply.github.com>
2020-11-14 02:55:23 +0900

    誤ってコピペしていた関数を削除

--- DlJava/src/net/digicre/digilaun/SplashFrame.java
+++ DlJava/src/net/digicre/digilaun/SplashFrame.java
@@ -78,22 +78,6 @@ public class SplashFrame extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SplashFrame frame = new SplashFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public SplashFrame() {

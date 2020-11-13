643988ab509c7721f8b9b64e853808d9f9e201ec
TkoolerLufar <19635219+TkoolerLufar@users.noreply.github.com>
2020-11-14 03:36:05 +0900

    設定アプリの.jarビルドコマンドを追加

--- DlJava/compileByJavaC.bat
+++ DlJava/compileByJavaC.bat
@@ -3,5 +3,6 @@ mkdir jbin jdoc
 javac   -d jbin -encoding UTF-8 -source 1.6 -target 1.6 src\net\digicre\digilaun\*.java src\net\digicre\digilaun\work\settable\*.java src\net\digicre\digilaun\work\*.java src\net\digicre\digilaun\config\*.java src\net\digicre\digilaun\config\regworks\*.java
 javadoc -d jdoc -encoding UTF-8 -source 1.6 -docencoding UTF-8 -author -private -sourcepath src -subpackages net.digicre.digilaun -quiet
 cd jbin
-jar cfe ..\DigiLaun.jar net.digicre.digilaun.DigiLaun .
+jar cfe ..\DigiLaun.jar net.digicre.digilaun.DigiLaun        net\digicre\digilaun\work\*.class net\digicre\digilaun\*.class
+jar cfe ..\config.jar   net.digicre.digilaun.config.DlConfig net\digicre\digilaun\work         net\digicre\digilaun\Config.class net\digicre\digilaun\Config$*.class net\digicre\digilaun\config
 PAUSE
